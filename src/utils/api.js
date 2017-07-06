6ed3a573df61f7eb68b73342cccfddd4dfe2f4ef
huangyi <huangyi@dejionline.com>
2017-07-07 01:21:34 +0800

    请求

---
+++ src/utils/api.js
@@ -0,0 +1,23 @@
+import vue from '../main';
+var token = sessionStorage.getItem("token");
+var obj = {
+  baseURL: 'http://123.206.232.11:8080/coachFront/api/front/',
+  auth: token
+};
+export  default{
+  post: (url, param)=> {
+    if (!token)
+      delete obj.auth
+    return new Promise((resolve, reject)=> {
+      this.axios.post(url, param, obj).then((r)=> {
+        if (r.data && r.data.businessCode == 100) {
+          r.data.returnData = JSON.parse(r.data.returnData);
+          return resolve({result: true, data: r.data.returnData})
+        } else {
+          return reject({result: false, msg: r.data.returnMsg})
+        }
+      })
+    })
+
+  }
+}
